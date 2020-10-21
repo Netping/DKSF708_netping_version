93c599623f50c6a201a54f2abd2c8c3a18db871c
smike-ru <sadmix@yandex.ru>
2020-10-21 11:04:56 -0700

    Change script to work with uci-compatible file format.

--- np_version.sh
+++ np_version.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 
-PROJECT_NAME="DKSF708"
-MAJOR_VERSION="1"
-MINOR_VERSION="0"
-DEVICE_NAME="NetPing SMS708"
-OPENWRT_VERSION="19"
+SETTINGS_FILE="/etc/config/netping-settings"
 BUILD_VERSION_FILE="/etc/build_ver"
-CONFIG_PARAMS="dhcp, ssh, luci"
 
-display_info() {
-  echo "Project name: $PROJECT_NAME"
-  echo "Project version: $MAJOR_VERSION.$MINOR_VERSION"
-  echo "Device name: $DEVICE_NAME"
-  echo "OpenWrt version: $OPENWRT_VERSION"
-  if test -f "$BUILD_VERSION_FILE"; then
-    build_ver=$(cat "$BUILD_VERSION_FILE")
-    echo "Build version: $build_ver"
-  else
-    echo "Build version: not found!"
-  fi
-  echo "Configuration parameters: $CONFIG_PARAMS"
-}
+if test -f "$BUILD_VERSION_FILE"; then
+  build_ver=$(cat "$BUILD_VERSION_FILE")
+  uci set netping-settings.software.build_time=$build_ver
+  uci commit
+else
+  uci set netping-settings.software.build_time="unknown"
+  uci commit
+fi
 
-display_info
+if [ $# -eq 0 ]; then
+  exit 0
+fi
+
+if [ "$1" == "--init" ]; then
+  #option device_serial_num '0000001'
+  serial_num=$(head -c 2 /dev/random | hexdump -e '16/1 "%04d""\n"')
+  uci set netping-settings.hardware.device_serial_num=$serial_num
+  uci commit
+else
+  echo "Unknown command line argument!"
+fi
+
